9b28c0c1cbeb21187822fcab28075a8b0deb557c
BoltlessRobot <luci.escalona@duocuc.cl>
2025-09-05 12:30:44 -0400

    clase JSON

--- ESCALONA/script.js
+++ ESCALONA/script.js
@@ -47,3 +47,50 @@ btnAgregar.addEventListener("click", ()=>{
         tareaInput.value = ""
     }
 })
+
+//Clase 2
+const btnCargar         = document.getElementById("btnCargar")
+const btnFiltrar        = document.getElementById("btnFiltrar")
+const productoContainer = document.getElementById("productoContainer")
+
+let productos = []
+
+function renderizarProductos(lista) {
+    productoContainer.innerHTML = ""
+    lista.forEach(producto => {
+        const col = document.createElement("div")
+        col.classList.add("col-md-4","col-lg-3")
+
+        col.innerHTML = `
+            <div class="card h-100 shadow-sm">
+                <img class="card-img-top" scr="${producto.imagen}" alt="${producto.nombre}">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text mb-1"><strong>Categoria:</strong> ${producto.categoria}</p>
+                    <p class="card-text"><strong>Precio:</strong> ${producto.precio}</p>
+                </div>
+            </div>
+        `
+        productoContainer.appendChild(col)
+    });
+}
+
+btnCargar.addEventListener("click", async ()=>{
+    try {
+        const respuesta = await fetch("data_base.json")
+        productos = await respuesta.json()
+        renderizarProductos(productos)
+    } catch (error) {
+        console.error("ERROR: no se pudo cargar los productos: ", error)
+    }
+});
+/*LOS TRES = EVALUAN QUE EL VALOR SEA EL QUE SE ESPECIFICA Y QUE COINCIDA CON EL TIPO, POR EJEMPLO
+ESTE DE ACA ABAJO VERIFICA QUE EL LARGO SEA 0 Y QUE SEA DE TIPO INT*/
+btnFiltrar.addEventListener("click", ()=>{
+    if(productos.length === 0){
+        alert("primero debes cargar los productos.")
+        return
+    }
+    const filtrados = productos.filter(producto => producto.categoria === "Torta")
+  renderizarProductos(filtrados)
+});
