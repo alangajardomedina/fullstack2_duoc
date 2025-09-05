d2e777b2aec3ad27fa13606cd35fa41a9c2f6dcf
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2025-09-05 12:28:46 -0400

    actualizacion - carga dinamica desde archivo

--- GAJARDO/script.js
+++ GAJARDO/script.js
@@ -47,3 +47,50 @@ btnAgregar.addEventListener("click", () => {
     tareaInput.value = "";
   }
 });
+
+// Clase 2: Consumo de JSON con Fetch:
+const btnCargar = document.getElementById("btnCargar")
+const btnFiltrar = document.getElementById("btnFiltrar")
+const productosContainer = document.getElementById("productosContainer")
+
+let productos = []
+
+//Vamos a crear una función capas de renderizar los productos en la página web:
+function renderizarProductos(lista){
+  productosContainer.innerHTML = ""
+  lista.forEach(p => {
+    const col = document.createElement("div")
+    col.classList.add("col-md-4","col-lg-3")
+    
+    col.innerHTML = `
+      <div class="card h-100 shadow-sm">
+        <img src="${p.imagen}" class="card-img-top" alt="${p.nombre}">
+        <div class="card-body">
+          <h5 class="card-title">${p.nombre}</h5>
+          <p class="card-text mb-1"><strong>Caterogia:</strong> ${p.categoria}</p>
+          <p class="card-text"><strong>Precio:</strong> ${p.precio}</p>
+        </div>
+      </div>
+    `
+    productosContainer.appendChild(col)
+  });
+}
+
+btnCargar.addEventListener("click", async ()=> {
+  try {
+    const respuesta = await fetch("db.json")
+    productos = await respuesta.json()
+    renderizarProductos(productos)
+  } catch (error) {
+    console.error("Error al cargar producto:",error)
+  }
+})
+
+btnFiltrar.addEventListener("click",()=>{
+  if(productos.length === 0){
+    alert("Primero debes cargar los productos.")
+    return
+  }
+  const filtrados = productos.filter(p => p.categoria === "Electrónica")
+  renderizarProductos(filtrados)
+})
