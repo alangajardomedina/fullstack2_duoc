0c19119c23a6d0e15010ce69f2384414b0f9b4b3
COdelValle <c.ormeno@duocuc.cl>
2025-09-12 11:19:32 -0300

    Validar cierre de sesión

--- ORMENO/src/assets/js/validar_session.js
+++ ORMENO/src/assets/js/validar_session.js
@@ -4,9 +4,16 @@ if(!token){
     window.location.href = "./../../login.html"
     alert("Tiene que iniciar sesión")
 }else{
-    const payload = JSON.parse(token)
+    const payload = JSON.parse(atob(token))
     if(Date.now() > payload.exp){
+        localStorage.removeItem("token")
         alert("Tu sesión ha expirado")
         window.location.href = "./../../login.html"
     }
 }
+
+const btn_logout = document.getElementById("btn_logout")
+btn_logout.addEventListener("click",()=>{
+    window.location.href("./../../../index.html")
+    localStorage.removeItem("token")
+})
